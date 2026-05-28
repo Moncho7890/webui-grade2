39af88a3c062a110df80006b2a50339950154fa5
Moncho7890 <ayemoncho111@gmail.com>
2026-05-28 15:58:12 +0900

    予約機能の追加、入力チェック機能の実装、おすすめ機能の追加を理解しました。

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function reserve() {
+    // 入力した内容を取得する
     const name = document.getElementById('guestName').value;
     const count = document.getElementById('guestCount').value;
 
-
-    const result= document.getElementById('reserveResult');
-    result.textContent = `ご予約ありがとうございます！お名前: ${name}, 人数: ${count}名`;}
+    // 結果を表示する場所を取得する
+    const result = document.getElementById('reserveResult');
+    // 名前か人数が空だったら、注意メッセージを出して終了する
+    if (name === '' || count === '') {
+        result.textContent = '入力してください';
+        return;
+    } else {
+        result.textContent = `✓ ご予約ありがとうございます、${name}様。${count}名様で承りました。`;
+    }
+}
+//recommend
+function pickRecommend() {
+}
